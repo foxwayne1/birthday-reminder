52fdc45dbc16d339cd0acfbfe74b4bdb375ee6c9
foxwayne1 <fox.wayne1@gmail.com>
2022-05-19 11:32:08 +0100

    base setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
+
+const App = () => {
+  return <h2>reminder project setup</h2>
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    name: 'Wayne Fox',
+    age: 44,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Lynn Fox',
+    age: 36,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 3,
+    name: 'Joanna Fox',
+    age: 18,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Connor Fox',
+    age: 17,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+]
